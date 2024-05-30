e87924b7f6062444af25ec4a3bc14e7ba1ba8f13
JaivinSavaliya <jpsavaliya14@gmail.com>
2024-05-31 00:27:43 +0530

    minor property change

--- client/src/Pages/Auth/Register.js
+++ client/src/Pages/Auth/Register.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Form from '../../Components/shared/form/Form'
+import React from "react";
+import Form from "../../Components/shared/form/Form";
 
 const Register = () => {
   return (
@@ -9,11 +9,15 @@ const Register = () => {
           <img src="./assets/images/banner2.jpg" alt="Register Image" />
         </div>
         <div className="col-md-4 form-container">
-          <Form submitButton={"Register"} formTitle={"Register"} />
+          <Form
+            submitButton={"Register"}
+            formTitle={"Register"}
+            formType={"register"}
+          />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
