2d710049d30a48b553c457fd620b7840f7fdb976
JaivinSavaliya <jpsavaliya14@gmail.com>
2024-06-25 23:09:06 +0530

    added reducer for registration

--- client/src/Redux/features/auth/authSlice.js
+++ client/src/Redux/features/auth/authSlice.js
@@ -1,11 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { userLogin } from "./authAction";
+import { userLogin, userRegister } from "./authAction";
+
+const token = localStorage.getItem("token") ? localStorage.getItem("token") : null;
 
 const initialState = {
     loading: false,
     error: null,
     user: null,
-    token: null
+    token,
 }
 
 const authSlice = createSlice({
@@ -13,6 +15,7 @@ const authSlice = createSlice({
     initialState: initialState,
     reducers: {},
     extraReducers: (builder) => {
+        //reducer for login user
         builder.addCase(userLogin.pending, (state) => {
             state.loading = true;
             state.error = null;
@@ -25,6 +28,22 @@ const authSlice = createSlice({
             state.token = payload.token;
         })
         builder.addCase(userLogin.rejected, (state, { payload }) => {
+            state.loading = false;
+            state.error = payload;
+        })
+
+        // reducer for register user
+        builder.addCase(userRegister.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        })
+        builder.addCase(userRegister.fulfilled, (state, { payload }) => {
+            state.loading = false;
+            // state.success = true;
+            state.error = null;
+            state.user = payload.user;
+        })
+        builder.addCase(userRegister.rejected, (state, { payload }) => {
             state.loading = false;
             state.error = payload;
         })
