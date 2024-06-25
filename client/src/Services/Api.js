707d7a03655971c5f9aea8fedb898c95da8d9373
JaivinSavaliya <jpsavaliya14@gmail.com>
2024-06-25 23:09:19 +0530

    minor changes

--- client/src/Services/Api.js
+++ client/src/Services/Api.js
@@ -6,9 +6,6 @@ const API = axios.create({
     baseURL: "http://localhost:4000/api/v1"
 })
 
-console.log("hiii");
-// console.log(process.env.REACT_BASE_URL)
-
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('token')){
         req.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
